ba91e97c8f7a830c518781c2aebdb6e4d5048f87
jules <nadberezny@gmail.com>
2018-08-06 23:15:48 +0200

    Eq instance for Option of Cat.

--- src/main/scala/nadberezny/typeclass/eq/instances.scala
+++ src/main/scala/nadberezny/typeclass/eq/instances.scala
@@ -5,4 +5,12 @@ import nadberezny.Cat
 
 object instances {
   implicit val catEq = Eq.instance[Cat] { (cat, otherCat) => cat == otherCat }
+
+  implicit def optionEq[A](implicit proof: Eq[A]): Eq[Option[A]] = (option: Option[A], otherOption: Option[A]) =>
+    (option, otherOption) match {
+      case (None, None) => true
+      case (None, Some(_)) => false
+      case (Some(_), None) => false
+      case (Some(a), Some(b)) => a == b
+  }
 }
--- src/test/scala/nadberezny/typeclass/instances/EqOfCatTest.scala
+++ src/test/scala/nadberezny/typeclass/instances/EqOfCatTest.scala
@@ -8,12 +8,22 @@ import nadberezny.Cat
 import nadberezny.typeclass.eq.instances._
 
 class EqOfCatTest extends FunSuite {
-  test("cats typesafe eq") {
-    val cat1 = Cat("lolo", 1, "grey")
-    val cat2 = Cat("olol", 2, "black")
+  val cat1 = Cat("lolo", 1, "grey")
+  val cat2 = Cat("olol", 2, "black")
 
+  test("cats typesafe eq") {
     assert(Eq.eqv(cat1, cat1))
     assert(!Eq.eqv(cat1, cat2))
+  }
+
+  test("eq of option of cats") {
+    val option1 = Option(cat1)
+    val option2 = Option(cat2)
+
+    assert(Eq.eqv(option1, option1))
+    assert(!Eq.eqv(option1, option2))
+    assert(Eq.eqv(Option.empty[Cat], Option.empty[Cat]))
+    assert(!Eq.eqv(Option(cat1), Option.empty[Cat]))
   }
 
 }
